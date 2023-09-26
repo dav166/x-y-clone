ad1a0cf4929a908279be8f6d5d1d2df23a999a7f
David <5992264+dav166@users.noreply.github.com>
2023-09-26 18:35:28 -0500

    feat: Refactor app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,9 +40,31 @@ const Home = () => {
     <div className = "w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header */}
-        <LeftSidebar/>
-        {/* <main>Home timeline</main>
-        <section>right sidebar</section> */}
+        <LeftSidebar />
+        <main className='ml-[275px] p-6 mx-2 flex w-[600px] h-full min-h-scren flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600'>
+          <h1 className='text-xl font-bold'>Home</h1>
+          <div className='border-t-[0.5px] border-b-[0.5px] border-gray-600 h-32 relative'>
+            <div className='w-10 h-10 bg-slate-400 rounded-full'></div>
+            <div className='flex flex-col'>
+              <div className='border-b-[0.5px] border-gray-600'>
+                <input 
+                  type='text' 
+                  className='w-full h-full bg-transparent outline-none border-none' 
+                  placeholder="What's happening?" 
+                />
+              </div>
+              <div className='w-full justify-between items-center flex'>
+                <div></div>
+                <div>
+                  <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+                    Tweet 
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        {/* <section>right sidebar</section> */}
       </div>
     </div>
   );
