d6b2d6519456dbd3c501331244c68d5eb13a5915
David <5992264+dav166@users.noreply.github.com>
2023-09-26 13:19:31 -0500

    feat: Init home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className = "w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        {/* left sidebar for navigation/header */}
+        <section className="fixed w-72 flex flex-col h-screen">
+          left sidebar
+        </section>
+        <main>Home timeline</main>
+        <section>right sidebar</section>
+      </div>
+    </div>
   )
 }
 
