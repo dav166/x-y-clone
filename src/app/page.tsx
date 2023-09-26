b616a8061a069d3ae96c348fda99f80ced36e80e
David <5992264+dav166@users.noreply.github.com>
2023-09-26 17:55:03 -0500

    feat: Create components folder and create sidebar companent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { BiHomeCircle, BiUser } from 'react-icons/bi'
 import { HiOutlineHashtag } from 'react-icons/hi'
 import { BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots } from 'react-icons/bs'
+import LeftSidebar from '@/components/LeftSidebar';
 
 const NAVIGATION_ITEMS = [
   {
@@ -32,49 +33,19 @@ const NAVIGATION_ITEMS = [
     title: 'Profile',
     icon: BiUser
   }
-]
+];
 
 const Home = () => {
   return (
     <div className = "w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header */}
-        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
-          <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
-            {NAVIGATION_ITEMS.map((item) => (
-              <Link
-                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
-                href={`/${item.title.toLowerCase()}`}
-                key={item.title}
-              >
-                <div>
-                  <item.icon />
-                </div>
-                {item.title !== "Twitter" && <div>{item.title}</div>}
-              </Link>
-            ))}
-            <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
-              Tweet 
-            </button>
-          </div>
-          <button className='rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-2xl text-center hover:bg-white/10 transition duration-200 w-full justify-between'>
-            <div className='flex items-center space-x-2'>
-              <div className='rounded-full bg-slate-400 w-12 h-12'></div>
-              <div className='text-left text-sm'>
-                <div className='font-semibold'>David</div>
-                <div className=''>@SpaulDav</div>
-              </div>
-            </div>
-            <div>
-              <BsThreeDots/>
-            </div>
-          </button>
-        </section>
+        <LeftSidebar/>
         {/* <main>Home timeline</main>
         <section>right sidebar</section> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
